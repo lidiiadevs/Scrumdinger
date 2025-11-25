ac6c7bb9a80c9e109ae7f536cff22b9c4a11347c
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-11-24 20:24:53 -0500

    add navigation between detailView and meetingView add "attendee" struct extension

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -11,7 +11,36 @@ struct DetailView: View {
     let scrum: DailyScrum
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .foregroundColor(.accentColor)
+                        .font(.headline)
+                }
+                HStack {
+                    Label("Lenght", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
     }
 }
 
