86f9307c424cd8adaeb93df6426aa5b0bc3906c7
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-11-25 01:47:47 -0500

    add DetailEditView

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Lidiia Diachkovskaia on 11/25/25.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    
+    var body: some View {
+        Form{
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+            }
+        }
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
