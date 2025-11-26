14310e07ec090031d874859f011e8d87f27c524f
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-11-26 16:12:28 -0500

    add ThemeView file for future picker

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,26 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Lidiia Diachkovskaia on 11/26/25.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct ThemeView: View { //You’ll use this theme view as a cell in a list to display all the themes to the user.
+    let theme: Theme
+    
+    var body: some View {
+        Text(theme.name)
+            .padding(4)
+            .frame(maxWidth: .infinity)
+            .background(theme.mainColor)
+            .foregroundColor(.accentColor)
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
+
+#Preview {
+    ThemeView(theme: .buttercup)
+}
